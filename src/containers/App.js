a4b882df05c0281debdae388bc2759530287ff5f
twolak88 <twolak88@gmail.com>
2021-02-25 21:42:25 +0100

    Setting State Correctly

--- src/containers/App.js
+++ src/containers/App.js
@@ -29,7 +29,8 @@ class App extends Component {
       { id: 'id3', name: 'Tom', age: 31 },
     ],
     showPersons: false,
-    showCockpit: true
+    showCockpit: true,
+    changeCounter: 0
   };
 
   static getDerivedStateFromProps(props, state) {
@@ -59,8 +60,11 @@ class App extends Component {
 
     const persons = [...this.state.persons];
     persons[personIndex] = person;
-    this.setState({
-      persons: persons
+    this.setState((prevState, props) => {
+      return {
+        persons: persons,
+        changeCounter: prevState.changeCounter + 1
+      }
     });
   };
 
